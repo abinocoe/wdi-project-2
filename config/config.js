bd2466978c1de3f0c48392f213436dbcf2a19b9e
abinocoe <abigailcoe@gmail.com>
2016-09-12 15:23:15 +0100

    basic authentication

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  db: "mongodb://localhost/findscraper"
+  db: "mongodb://localhost/findscraper",
+  port: process.env.PORT || 3000
 };
--- index.js
+++ index.js
@@ -1,55 +1,19 @@
-const rp         = require('request-promise');
-const cheerio    = require('cheerio');
+const express    = require('express');
+const morgan     = require('morgan');
+const bodyParser = require('body-parser');
+const cors       = require('cors');
 const mongoose   = require('mongoose');
+
+const app        = express();
 const config     = require('./config/config');
-const Find       = require('./models/find');
-let url          = "https://finds.org.uk/database/search/results/bbox/51.261915%2C-0.698833%2C51.713416%2C0.388813/show/100/thumbnail/1";
-let urlend       = "/format/json";
-let count        = 0;
+const router     = require('./config/routes');
 
 mongoose.connect(config.db);
 
-Find.collection.drop();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/api', router);
 
-
-
-rp(`${url}${urlend}`)
-  .then((body, response) => {
-    const $ = cheerio.load(body);
-    let data = JSON.parse(body);
-    const pages = (parseInt((data.meta.totalResults)/100+1));
-    //console.log(pages);
-    for (var j = 1; j <= pages; j++) {
-      console.log(j);
-
-    rp(`${url}/page/${j}${urlend}`)
-    .then((body, response) => {
-    const shrt = $(data.results);
-    shrt.each((i, result) => {
-      let object = {
-              objectType:  shrt[i].objecttype,
-              broadPeriod: shrt[i].broadperiod,
-              description: shrt[i].description,
-              inscription: shrt[i].inscription,
-              mintName: shrt[i].mintname,
-              district: shrt[i].district,
-              parish: shrt[i].parish,
-              lat: shrt[i].fourFigureLat,
-              lng: shrt[i].fourFigureLon,
-              fromDate: shrt[  i].fromdate,
-              todate: shrt[i].todate,
-              imageURL: `https://finds.org.uk/${shrt[i].imagedir}medium/${shrt[i].filename}`,
-              discoveryMethod: shrt[i].discoveryMethod,
-              subsequently: shrt[i].subsequentActionTerm
-      };
-      Find.create(object, (err, find) => {
-        if (err) return console.error(err);
-              count++;
-              return console.log(`${count}: ${find.objectType} was saved.`);
-      });
-    });
-  })
-  .catch(console.error);
-}
-})
-.catch(console.error);
+app.listen(config.port, () => console.log(`WooooHoooo on port ${config.port}`));
