3c8f584f4b9d78711c94fbc246179e3c4591939b
abinocoe <abigailcoe@gmail.com>
2016-09-16 10:57:12 +0100

    Added mongolab to config

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: "mongodb://localhost/findscraper",
+  db: process.env.MONGOLAB_URI || "mongodb://localhost/findscraper",
   port: process.env.PORT || 3000,
   secret: process.env.SECRET || "Tell anyone and I'll cut you."
 };
